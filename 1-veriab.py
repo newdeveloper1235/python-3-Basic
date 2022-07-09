59e230dc0f306858a5e8041e48107e41e823f38f
newdeveloper1235 <sahelmariam223@gmail.com>
2022-07-09 11:59:59 +0300

    Add Basic

---
+++ 1-veriab.py
@@ -0,0 +1,104 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print('Hi)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print('Hi')
+      
+Hi
+4+3
+      
+7
+x=5
+      
+x
+      
+5
+x
+      
+5
+x_y=10;x_y
+      
+10
+ True False as if return
+      
+SyntaxError: unexpected indent
+x,y=3,4
+      
+x
+      
+3
+y
+      
+4
+type(x)
+      
+<class 'int'>
+s='ali'; type(s)
+      
+<class 'str'>
+j=15 ;j
+      
+15
+a=3;b=2
+      
+a+b;a-b;a*b;a/b
+      
+5
+1
+6
+1.5
+a**b;a//b;a%b
+      
+9
+1
+1
+a-b
+      
+1
+5==6
+      
+False
+5>6;5<10
+      
+False
+True
+5!=6
+      
+True
+5>=4
+      
+True
+5<=3
+      
+False
+x=5;x=x+1;x
+      
+6
+x+=1
+      
+x
+      
+7
+x*=10
+      
+x
+      
+70
+x/=5;x
+      
+14.0
+x-=1
+      
+x
+      
+13.0
+x%=3;x
+      
+1.0
+x=10;x
+      
+10
+x/=2;x
+      
+5.0
---
+++ 2-Conditions.py
@@ -0,0 +1,75 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#condations
+username='ali'#variable
+if username=='ali':  # end with colon:
+    print('welom ali')
+elif username=='Maha': # second condition
+    print('Hi Maha')
+else print('you have to reset your pass word')
+SyntaxError: expected ':'
+if username=='ali':  # end with colon:
+    print('welom ali')
+elif username=='Maha': # second condition
+    print('Hi Maha')
+else:
+    print('you have to reset your pass word')
+ali
+SyntaxError: invalid syntax
+cls
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    cls
+NameError: name 'cls' is not defined
+username='ali'
+if username == 'ali':
+    print('Welcome ali')
+
+Welcome ali
+Welcome ali
+SyntaxError: invalid syntax
+
+if username=='ali':  # end with colon:
+    print('welom ali')
+elif username=='Maha': # second condition
+    print('Hi Maha')
+else:
+    print('you have to reset your pass word')
+
+welom ali
+if username=='ali':  # end with colon:
+    print('wellcom ali')
+elif username=='Maha': # second condition
+    print('Hi Maha')
+else:
+    print('you have to reset your pass word')
+
+wellcom ali
+username='Maha'
+
+if username=='ali':  # end with colon:
+    print('wellcom ali')
+elif username=='Maha': # second condition
+    print('Hi Maha')
+else:
+    print('you have to reset your pass word')
+
+Hi Maha
+username='Mona'
+if username=='ali':  # end with colon:
+    print('wellcom ali')
+elif username=='Maha': # second condition
+    print('Hi Maha')
+else:
+    print('you have to reset your pass word')
+
+you have to reset your pass word
+username='ali'
+if username=='ali':  # end with colon:
+    print('wellcom ali')
+elif username=='Maha': # second condition
+    print('Hi Maha')
+else:
+    print('you have to reset your pass word')
+
+you have to reset your pass word
